bc0933d89f3d9533636b090630d813f0ad3674e0
arun200011 <118739698+arun200011@users.noreply.github.com>
2023-02-13 11:16:41 +0530

    Update LAB3.java

--- LAB-3/LAB3.java
+++ LAB-3/LAB3.java
@@ -1,1 +1,81 @@
+import java.util.Scanner;
 
+public class LAB3 {
+  //creating array
+  static final int max_students = 1024;
+  static String[] studentNames = new String[max_students];
+  static String[][] studentDetails = new String[max_students][5];
+  
+  public static void main(String[] args) {
+    //taking input
+    int choice, numOfStudents = 0;
+    Scanner input = new Scanner(System.in);
+    
+    while (true) {
+      //asking choices from user
+      System.out.println("1. Add Student");
+      System.out.println("2. View Student Details");
+      System.out.println("3. Exit");
+      System.out.print("Enter your choice: ");
+      //using switch cases
+      
+      choice = input.nextInt();
+      switch (choice) {
+        case 1:
+          addStudent(numOfStudents++);
+          break;
+        case 2:
+          viewStudentDetails();
+          break;
+        case 3:
+          System.exit(0);
+        default:
+          System.out.println("Invalid choice! Please try again.");
+      }
+    }
+  }
+  //creating class for adding student
+  public static void addStudent(int numOfStudents) {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("Enter student name: ");
+    studentNames[numOfStudents] = scan.nextLine();
+    System.out.print("Enter student register number: ");
+    studentDetails[numOfStudents][0] = scan.nextLine();
+    System.out.print("Enter student email: ");
+    studentDetails[numOfStudents][1] = scan.nextLine();
+    System.out.print("Enter student class: ");
+    studentDetails[numOfStudents][2] = scan.nextLine();
+    System.out.print("Enter student department: ");
+    studentDetails[numOfStudents][3] = scan.nextLine();
+    
+    System.out.println("Student details added successfully!");
+  }
+  
+  //creating class for viewing student details
+  
+  public static void viewStudentDetails() {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("Enter student name to view details: ");
+    String name = scan.nextLine();
+    
+    int index = -1;
+    for (int i = 0; i < studentNames.length; i++) {
+      if (studentNames[i] != null && studentNames[i].equals(name)) {
+        index = i;
+        break;
+      }
+    }
+    
+    if (index != -1) {
+      System.out.println("Name: " + studentNames[index]);
+      System.out.println("Register Number: " + studentDetails[index][0]);
+      System.out.println("Email: " + studentDetails[index][1]);
+      System.out.println("Class: " + studentDetails[index][2]);
+      System.out.println("Department: " + studentDetails[index][3]);
+    } else {
+      System.out.println("Student name not found!");
+    }
+  }
+}
