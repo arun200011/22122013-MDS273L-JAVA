0d36696700bc0a473e1a705db5abbcbcded44a08
arun200011 <118739698+arun200011@users.noreply.github.com>
2023-05-08 12:03:37 +0530

    Update LAB5.java

--- LAB-5/LAB5.java
+++ LAB-5/LAB5.java
@@ -1,87 +1,108 @@
-import java.util.*;
-class LAB5{
-  //function for encrypting
+import java.util.Arrays;
+import java.util.Scanner;
 
-    static String encrypt(String s){
-        String temp="";
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch==' '){
-                temp=temp+ch;
+public class LAB5 {
 
-            }
-            else{
-                int a=(int)ch;
-                //System.out.println(a);
-                if(a<=67){
-                    a=(a-2)+25;
-                }
-                else {a=a-3;}
-                temp=temp + (char)(a);
-            }
+    static char encode(){
 
-           
-    }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter String input : ");
 
-    return temp;}
-  
-  //function for decrypting
+        String inp = scan.nextLine();
 
-    static String decrypt(String s){
-        String temp="";
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch==' '){
-                temp=temp+ch;
-
-            }
-            else{
-                int a=(int)ch;
-                //System.out.println(a);
-                if(a>=88){
-                    a=a-23;
-                }
-                else {a=a+3;}
-                temp=temp + (char)(a);
-            }
-
-           
-    }
-
-    return temp;}
-
-
-
-
-    public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
-        int n;
-        boolean flag=false;
-        do{
-            System.out.println("*********MENU*****");
-            System.out.println("1.Encrypt a message");
-            System.out.println("2.Decrypt a message");
-            System.out.print("Enter your choice: ");
-            n=Integer.parseInt(sc.nextLine());
-            if (n==1){
-                flag=true;
-                System.out.println("Enter a string: ");
-                String s=sc.nextLine();
-                String str= encrypt(s);
-                System.out.println("The encypted text is"+str);
-
-            }
-            if(n==2){
-                    flag=true;
-                    System.out.println("Enter a string: ");
-                    String s=sc.nextLine();
-                    String str= decrypt(s);
-                    System.out.println("The decrpted text is"+str);
-    
-                }
-            else flag=false;
-
-
+        char [] arr = new char[50];
         
-    }while(flag ==true);
-}}
+        for (int i = 0; i < inp.length(); i++) {
+            char inp1 = Character.toUpperCase(inp.charAt(i));
+            arr[i] = inp1;
+        }
+
+        char [] XX = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        char [] YY = {'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'};
+
+        for (int j = 0; j < arr.length; j++) {
+            for (int k = 0; k < XX.length; k++) {
+                char y = XX[k];
+                if(arr[j] == y){
+                    System.out.print(YY[k]);
+                }
+                else{
+                    continue;
+                }
+            }
+            if(arr[j] == ' '){
+                System.out.print(" ");
+            }
+        }
+        return 0;
+            
+        }
+        
+        static char decode(){
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter String input : ");
+    
+            String inp0 = scan.nextLine();
+    
+            char [] arr0 = new char[50];
+            
+            for (int i = 0; i < inp0.length(); i++) {
+                char inp01 = Character.toUpperCase(inp0.charAt(i));
+                arr0[i] = inp01;
+            }
+    
+            char [] XX0 = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+            char [] YY0 = {'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'};
+    
+            for (int j = 0; j < arr0.length; j++) {
+                for (int k = 0; k < XX0.length; k++) {
+                    char y0 = YY0[k];
+                    if(arr0[j] == y0){
+                        System.out.print(XX0[k]);
+                    }else{
+                        continue;
+                    }
+                }
+                
+            }
+        return ';';
+
+    }
+
+
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        
+        boolean exitt = true;
+
+        do{
+
+            System.out.println("Enter your choice");
+            System.out.println("*********MENU***************8");
+            System.out.println("****1. encode text******");
+            System.out.println("****2. deocde texte*****");
+            System.out.println("****3.exit**************");
+            Scanner scan = new Scanner(System.in);
+            int enter = scan.nextInt();
+
+            switch(enter){
+                case 1 :
+                encode();
+                break;
+
+                case 2:
+                decode();
+                break;
+
+                case 3:
+                exitt = false;
+            }
+            
+        }while(exitt);
+    }
+    
+}
