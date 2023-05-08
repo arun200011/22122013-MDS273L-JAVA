4308b97fc56186836b1c4481017a4552d0e7bc92
arun200011 <118739698+arun200011@users.noreply.github.com>
2023-05-08 20:22:08 +0530

    Update LAB7.java

--- LAB-7/LAB7.java
+++ LAB-7/LAB7.java
@@ -1,206 +1,204 @@
 import java.io.*;
 import java.util.*;
-public class LAB7 {
-    
-    // A method that sorts an array of floats in ascending order and returns the sorted array.
-    static float[] sort(float[] arr, int l) {
-        for (int i = 0; i < l; i++) {
-            for (int j = 0; j < l - i - 1; j++) {
-                if (arr[j] >= arr[j + 1]) {
-                    float temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+
+public class lab7{
+
+    public static float max(float[] a){
+        float max_val = -100.f;
+        for(float k:a){
+            if(k > max_val){
+                max_val = k;
             }
         }
-        return (arr);
+        return max_val;
     }
-    // A method that calculates and returns the mean of an array of floats.
-    static float mean(float[] arr, int l) {
-        float sum = 0;
-        for (int i = 0; i < l; i++) {
-            sum += arr[i];
+
+    public static float min(float[] a){
+        float min_val = 1000.f;
+        for(float k:a){
+            if(k < min_val){
+                min_val = k;
+            }
         }
-        float mean = sum / l;
+        return min_val;
+    }
+
+    public static float mean(float[] a){
+        float sum = 0;
+        for(float i: a){
+            sum += i;
+        }
+        float mean = sum/(a.length);
         return mean;
     }
-    // A method that calculates and returns the median of an array of floats.
-    static float median(float[] arr1, int l) {
-        float[] arr = sort(arr1, l);
-        float median = 0;
-        for (int i = 0; i < l; i++) {
-            if (l % 2 == 0) {
-                float m = (arr[(l / 2) - 1] + arr[(l / 2)]);
-                median = m / 2;
-            } else {
-                median = arr[(l - 1) / 2];
-            }
-        }
-        return median;
-    }
-    // A method that calculates and returns the mode of an array of floats.
-    static float mode(float[] arr, int l) {
-        int max = 0;
-        float mode = 0;
 
-        for (int i = 0; i < l; i++) {
-            int count = 0;
-            for (int j = i + 1; j < l; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
+    public static int[] frequency(float[] arr ){
+        int [] fr = new int [arr.length];
+        int visited = -1;
+        for(int i = 0; i < arr.length; i++){
+            int count = 1;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;            //To avoid counting same element again
+                    fr[j] = visited;
                 }
             }
-            if (count > max) {
-                max = count;
-                mode = arr[i];
+            if(fr[i] != visited)
+            fr[i] = count;
+        }
+        return fr;
+    }
+
+    public static float mode(float[] a){
+        int[] fr= frequency(a);
+        int max = 0;
+        int max_index = 0;
+        for(int i = 0; i < fr.length; i++){  
+            if(fr[i] != -1){
+                if(max > fr[i]){
+                    max = fr[i];
+                    max_index = i;
+                }
             }
-        }
-        if (max == 1) {
-            mode = 0;
-        }
-        return mode;
+        }	
+        return a[max_index];
     }
-    // A method that returns the maximum value in an array of floats.
-    static float max(float[] arr, int l) {
-        float[] arr1 = sort(arr, l);
-        float max = arr1[l - 1];
-        return max;
-    }
-    // A method that returns the minimum value in an array of floats.
-    static float min(float[] arr, int l) {
-        float[] arr1 = sort(arr, l);
-        float min = arr1[0];
-        return min;
-    }
-    // A method that calculates and returns a summary of statistical information for an array of arrays of floats.
-    static String[][] summary(float[][] para, int l) {
-        String[][] arr = new String[5][5];
-        String[] arr1 = { "MEAN", "MEDIAN", "MODE", "MAX", "MIN" };
-        arr[0] = arr1;
-        for (int i = 0; i < 4; i++) {
-            String[] sum = { mean(para[i], l) + "", median(para[i], l) + "", mode(para[i], l) + "", max(para[i], l) + "",
-                    min(para[i], l) + "" };
-            arr[i + 1] = sum;
+
+    public static float[] sort(float[] arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i + 1; j < arr.length; j++){
+                float tmp = 0;
+                if (arr[i] > arr[j]){
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
         }
         return arr;
     }
-// Define a static method named "display" that takes in a 2D string array and a string argument, and returns a string value
-static String display(String[][] arr, String s) {
-    // Define a string array named "arr1" and initialize it with values
-    String[] arr1 = {"PARAMETER", "SEPAL LENGTH", "SEPAL WIDTH", "PETAL LENGTH", "PETAL WIDTH"};
-    // Define a string variable named "b" and initialize it with a vertical bar character
-    String b = "|";
-    // Define a string variable named "str" and initialize it with a formatted string
-    String str = String.format("%s%s%s", "-".repeat(40), s, "-".repeat(40));
-    // Add a new line character to "str"
-    str += "\n";
-    // Loop through the elements in "arr1" array
-    for (int i = 0; i < 5; i++) {
-        // Append a vertical bar character and a formatted string to "str"
-        str += b + String.format("%-13s", arr1[i]) + b;
-        // Loop through the elements in the current row of "arr" array
-        for (int j = 0; j < 5; j++) {
-            // Append a formatted string to "str"
-            str += String.format("%-13s", arr[i][j]) + b;
+    
+    public static float median(float[] a){
+        float[] sorted_array = sort(a);
+        if(sorted_array.length%2 == 1){
+            return (float)sorted_array[a.length/2];
         }
-        // Add a new line character to "str"
-        str += "\n";
+        else{
+            return (float)(sorted_array[a.length/2] + sorted_array[a.length/2 +1])/2;  
+        }
     }
-    // Print the final string value to the console
-    System.out.println(str);
-    // Return the final string value
-    return str;
-}
 
-// Define the main method
-public static void main(String[] args) {
-    // Define a 2D string array named "arr" and initialize it with a size of 1024 x 6
-    String[][] arr = new String[1024][6];
-    // Define an integer variable named "n" and initialize it to zero
-    int n = 0;
-    try {
-        // Create a new file object with the file name "Iris.csv"
-        File ob = new File("Iris.csv");
-        // Create a new scanner object to read the file
-        Scanner scan = new Scanner(ob);
-        // Loop through each line in the file
-        while (scan.hasNextLine()) {
-            // Read the current line as a string
-            String s = scan.nextLine();
-            // Split the string into an array of strings using comma as the delimiter
-            arr[n] = s.split(",");
-            // Increment "n" by one
-            n++;
+    public static void display(float[] sl,float[] sw,float[] pl,float[] pw) {
+    
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|         SEPAL LENGTH           |%n");
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|%-16s|%-15s|%n","Mean",mean(sl));
+        System.out.printf("|%-16s|%-15s|%n","Median",median(sl));
+        System.out.printf("|%-16s|%-15s|%n","Mode",mode(sl));
+        System.out.printf("|%-16s|%-15s|%n","Max",max(sl));
+        System.out.printf("|%-16s|%-15s|%n","Min",min(sl));
+        System.out.printf("|--------------------------------|%n");
+
+        
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|         SEPAL WIDTH            |%n");
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|%-16s|%-15s|%n","Mean",mean(sw));
+        System.out.printf("|%-16s|%-15s|%n","Median",median(sw));
+        System.out.printf("|%-16s|%-15s|%n","Mode",mode(sw));
+        System.out.printf("|%-16s|%-15s|%n","Max",max(sw));
+        System.out.printf("|%-16s|%-15s|%n","Min",min(sw));
+        System.out.printf("|--------------------------------|%n");
+
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|         PETAL LENGTH           |%n");
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|%-16s|%-15s|%n","Mean",mean(pl));
+        System.out.printf("|%-16s|%-15s|%n","Median",median(pl));
+        System.out.printf("|%-16s|%-15s|%n","Mode",mode(pl));
+        System.out.printf("|%-16s|%-15s|%n","Max",max(pl));
+        System.out.printf("|%-16s|%-15s|%n","Min",min(pl));
+        System.out.printf("|--------------------------------|%n");
+
+
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|         PETAL WIDTH            |%n");
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|%-16s|%-15s|%n","Mean",mean(pw));
+        System.out.printf("|%-16s|%-15s|%n","Median",median(pw));
+        System.out.printf("|%-16s|%-15s|%n","Mode",mode(pw));
+        System.out.printf("|%-16s|%-15s|%n","Max",max(pw));
+        System.out.printf("|%-16s|%-15s|%n","Min",min(pw));
+        System.out.printf("|--------------------------------|%n");
+    }  
+    
+    public static void main(String[] args) {
+        try{
+        File csv = new File("iris.csv");
+        Scanner sc = new Scanner(csv);
+        sc.nextLine();
+        sc.useDelimiter(",");
+        float[] sl = new float[150];
+        float[] sw = new float[150];
+        float[] pl = new float[150];
+        float[] pw = new float[150];
+        int k = 0;
+    // System.out.println(sc.next());
+    // System.out.println(sc.nextFloat());
+    // System.out.println(sc.nextFloat());
+    //  System.out.println(sc.nextFloat()); 
+    //  System.out.println(sc.nextFloat());
+    //    System.out.println(sc.nextLine());
+
+        while(sc.hasNextLine()){
+        sc.next();
+        sl[k] = Float.parseFloat(sc.next());
+        sw[k] = Float.parseFloat(sc.next());
+        pl[k] = Float.parseFloat(sc.next());
+        pw[k] = Float.parseFloat(sc.next());
+        sc.nextLine();
+        k++;
         }
-        // Close the scanner object
-        scan.close();
-    } catch (Exception io) {
-        // Print the error message to the console if an exception occurs
-        System.out.println(io.getMessage());
-    }
-    // Decrement "n" by one
-    n--;
-    // Define four float arrays named "seplen", "sepwid", "petlen", and "petwid" with a size of "n"
-    float[] seplen = new float[n];
-    float[] sepwid = new float[n];
-    float[] petlen = new float[n];
-    float[] petwid = new float[n];
-    // Define a string array named "species" with a size of "n"
-    String[] species = new String[n];
-    // Define a 2D float array named "total" and initialize it with the four float arrays
-    float[][] total = {seplen, sepwid, petlen, petwid};
-    // Define three 2D float arrays named "setosa", "versicolor", and "virginica"
-    float[][] setosa =new float[4][n]; // 2D array to store data for setosa
-    float[][] versicolor =new float[4][n]; // 2D array to store data for versicolor
-    float[][] virginica =new float[4][n]; // 2D array to store data for virginica
-    for(int i = 0; i < n; i++){
-        // extract the four measurements for the ith flower
-        seplen[i] = Float.parseFloat(arr[i + 1][1]);
-        sepwid[i] = Float.parseFloat(arr[i + 1][2]);
-        petlen[i] = Float.parseFloat(arr[i + 1][3]);
-        petwid[i] = Float.parseFloat(arr[i + 1][4]);
-        // extract the species for the ith flower
-        species[i] = arr[i + 1][5].split("-")[1];
-    }
-    int setCount = 0; // initialize counter for setosa
-    int verCount = 0; // initialize counter for versicolor
-    int virCount = 0; // initialize counter for virginica
-    for(int i = 0; i < n; i++){
-        if(species[i].equals("setosa")){
-            // store the four measurements for the ith setosa flower
-            for(int j = 0; j < 4; j++){
-                setosa[j][setCount] = total[j][i];
+        sc.close();
+        
+        System.out.printf("|--------------------------------|%n");
+        System.out.printf("|       The Total Statistics     |%n");
+        System.out.printf("|--------------------------------|%n");
+
+        display(sl,sw,pl,pw);
+        Scanner scan = new Scanner(csv);
+        scan.nextLine();
+        scan.useDelimiter(",");
+        String s[] = {"SETOSA" , "VERSICOLOR" , "VIRGINICA"};
+        // int push = 0;
+
+        float[] sl1 = new float[50];
+        float[] sw1 = new float[50];
+        float[] pl1 = new float[50];
+        float[] pw1 = new float[50];
+
+        for(String o:s){
+
+            System.out.printf("|--------------------------------|%n");
+            System.out.printf("|%-9s%-23s|%n","",o);
+            System.out.printf("|--------------------------------|%n");
+
+            for(int i = 0;i<50;i++){
+
+                scan.next();
+                sl1[i] = Float.parseFloat(scan.next());
+                sw1[i] = Float.parseFloat(scan.next());
+                pl1[i] = Float.parseFloat(scan.next());
+                pw1[i] = Float.parseFloat(scan.next());
+                scan.nextLine();
             }
-            setCount++; // increment count for setosa
+            display(sl1,sw1,pl1,pw1);
+            scan.close();
         }
-        else if(species[i].equals("versicolor")){
-            // store the four measurements for the ith versicolor flower
-            for(int j = 0; j < 4; j++){
-                versicolor[j][verCount] = total[j][i];
-            }
-            verCount++; // increment count for versicolor
         }
-        else if(species[i].equals("virginica")){
-            // store the four measurements for the ith virginica flower
-            for(int j = 0; j < 4; j++){
-                virginica[j][virCount] = total[j][i];
-            }
-            virCount++; // increment count for virginica
+        catch(Exception e){
+        System.out.println(e.getMessage());
         }
     }
-    // generate the summary table for each species and the total
-    String str = display(summary(total,n),"TOTAL")+display(summary(setosa,setCount),"SETOSA")+
-                    display(summary(versicolor,verCount),"VERSICOLOR")+
-                    display(summary(virginica,virCount),"VIRGINICA");
-    // write the summary table to a file
-    try {
-        FileWriter writer = new FileWriter("Output.txt");
-        writer.write(str);
-        writer.close();
-        System.out.println("Output written to file 'output.txt'");
-    } catch (IOException e) {
-        System.out.println("An error occurred while writing to file.");
-        e.printStackTrace();
-    }
-}
 }
